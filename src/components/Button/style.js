e5ae5dbf0161ee36e321e2fe320c4a2b87338a0a
Acopa123 <jose_acopa@hotmail.com>
2017-07-19 16:27:34 -0500

    JSX tranporte

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,19 +1,13 @@
 import styled from 'styled-components';
 
-const ButtonBlue = styled.a`
-  background-color: #2980b9;
-  color: #ffffff;
-  border-radius: 6px;
-  position: absolute;
-  margin: -35px 0px 0px 10px;
-  padding: 16px 0px 16px 0px;
-  width: 230px;
-  text-align: center;
-
-  &:hover{
-    color: #ffffff;
-    background-color: #3498db;
-  }
+const ButtonBlue = styled.button`
+  width:100px;
+  height:40px;
+  background-color:#005caf;
+  border:none;
+  border-radius:5px;
+  color:white;
+  box-shadow:2px 2px 6px black;
 `;
 
 export {ButtonBlue};
