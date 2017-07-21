f30a85d8cb2d0b9fe779e075202367c1e5e954f2
Antonio Ortega <aortega@creatuviaje.com>
2017-07-21 12:41:02 -0500

    Without styled-component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,35 +4,35 @@ import styled from 'styled-components'
 
 
 function Input(props){
-  let textInput= null
-  const ContainerInput= styled.div`
-    width:48%;
-  `;
 
-  const Inputs=styled.input`
-    width:${props.type ? '50%' : '100%'};
-    height:${props.type ? 'auto' : '35px'};
-    color:#6E6E6E;
-    border-radius:5px;
-    border:none;
-    box-shadow:1px 1px 5px #2E2E2E inset;
-    padding-left:${props.type ? '0' : '10px'};
-    margin-top:10px;
-    display:flex;
-    flex-direction:column;
-  `;
+  const ContainerInput= {
+    width:'48%'
+  }
 
-  const Label = styled.label`
-    color:black;
-    font-size:16px;
-    font-family:Times;
-  `;
+  const Inputs={
+    width:props.type ? '50%' : '100%',
+    height:props.type ? 'auto' : '35px',
+    color:'#6E6E6E',
+    borderRadius:'5px',
+    border:'none',
+    boxShadow:'1px 1px 5px #2E2E2E inset',
+    paddingLeft:props.type ? '0' : '10px',
+    marginTop:'10px',
+    display:'flex',
+    flexDirection:'column',
+  }
+
+  const Label = {
+    color:'black',
+    fontSize:'16px',
+    fontfamily:'Times',
+  }
 
   return(
-    <ContainerInput>
-      <Label htmlFor={props.Id}>{props.text}</Label>
-      <Inputs placeholder={props.placeholder} name={props.name} id={props.Id} type={props.type} ref={props.inputRef}/>
-    </ContainerInput>
+    <div style={ContainerInput}>
+      <label style={Label} htmlFor={props.Id}>{props.text}</label>
+      <input style={Inputs} placeholder={props.placeholder} name={props.name} id={props.Id} type={props.type} ref={props.inputRef}/>
+    </div>
   )
 }
 
