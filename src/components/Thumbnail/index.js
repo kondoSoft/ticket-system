b356726dd9377aaaa6750fb1ad516b5c025e1e4c
Acopa123 <jose_acopa@hotmail.com>
2017-07-21 16:33:55 -0500

    add date on thumbnail

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -4,10 +4,13 @@ import { Div, Img, P } from './style';
 import Button from '../ButtonBlue';
 
 function Thumbnail (props) {
+
   return (
     <Div onClick={()=> props.setUI(props.elements.key)}>
       <Button name={props.elements.title}/>
       <Img src={props.elements.image}/>
+      {props.elements.price ? <P>Price: ${props.elements.price}</P> : ''}
+      {props.elements.address ? <P>Address: {props.elements.address}</P> : ''}
     </Div>
   )
 }
