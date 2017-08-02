c7b1cfc7bc356ac5d47910ef237c8feba0f69aab
Acopa123 <jose_acopa@hotmail.com>
2017-08-02 10:01:50 -0500

    add formPay

---
+++ src/components/formPay/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background: rgba(255,255,255,0.9);
+  margin-top: 20px;
+  padding: 20px;
+  border-radius: 6px;
+  color: #666;
+  font-family: sans-serif;
+  font-size: 18px;
+`;
+
+export {Div}
